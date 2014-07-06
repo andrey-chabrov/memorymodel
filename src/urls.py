9d2f4630fdc4808b0d91dbfaa18389edb7a31d95
andrey-chabrov <idronium@gmail.com>
2014-07-07 00:46:24 +0700

    Urls were tuned.

--- src/urls.py
+++ src/urls.py
@@ -1,12 +1,22 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+import settings
+
+import memorymodel.urls
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'memorymodel.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(memorymodel.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^{0}(?P<path>.*)$'.format(settings.MEDIA_URL[1:]),
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT,
+             'show_indexes': True}),
+    )
