5e188351f31c7a0a40db8f9ab0fcff75ef389dbf
andrey_chabrov <idronium@gmail.com>
2014-07-16 14:42:38 +0700

    Formset treatment has been almost moved to "edit" view.

--- src/memorymodel/urls.py
+++ src/memorymodel/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('memorymodel.views',
     url(r'^$', 'home', name='home'),
-    url(r'^model/(?P<modelname>[a-z0-9_]+)/$', 'home', name='model'),
     url(r'^ajax/edit/(?P<modelname>[a-z0-9_]+)/$', 'edit', name='edit'),
 )
--- src/memorymodel/views.py
+++ src/memorymodel/views.py
@@ -3,7 +3,6 @@
 from annoying import decorators
 
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
 from django.forms.models import modelformset_factory
 from django.forms.widgets import DateInput
 from django.forms.widgets import HiddenInput
@@ -17,29 +16,16 @@ import memorymodel
 
 
 @decorators.render_to('home.html')
-def home(request, modelname=None):
+def home(request):
 
-    models = ((model_name, model._meta.verbose_name, model) for model_name, model in
-        ((model_name, getattr(memorymodel.models, model_name)) for model_name in 
-        ContentType.objects.filter(app_label='memorymodel').values_list(
-        'model', flat=True)))
-
-    model = modelname and getattr(memorymodel.models, modelname, None)
-    if modelname is not None and model is None:
-        raise Http404()
-
-    formset = model and modelformset_factory(model)(request.POST or None)
-
-    if request.method == 'POST':
-        if formset.is_valid():
-            formset.save()
-            return redirect(reverse('model', kwargs={'modelname': modelname}))
+    models = (
+        (model_name, getattr(memorymodel.models, model_name)._meta.verbose_name) 
+        for model_name in ContentType.objects.filter(
+            app_label='memorymodel').values_list('model', flat=True))
 
     return {
         'pagetitle': u'Tables',
         'models': models,
-        'formset': formset,
-        'verbose_name': model and model._meta.verbose_name,
     }
 
 
