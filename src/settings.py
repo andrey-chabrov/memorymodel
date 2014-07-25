6562b958d4fccd27ffc6eb7c2580eebf20beb4d4
andrey_chabrov <idronium@gmail.com>
2014-07-25 14:51:25 +0700

    "MEDIA_URL" has been changed with "STATIC_URL" to production purpose.

--- src/settings.py
+++ src/settings.py
@@ -84,14 +84,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/site-media/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/site-media/'
+STATICFILES_DIRS = (MEDIA_ROOT,)
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
 
